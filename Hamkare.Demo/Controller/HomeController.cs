d2a4e3cf73f7a1313912224b29eeee526535fdc6
Mrbanad <mrbanad@outlook.com>
2025-12-29 20:25:18 +0330

    Add Pages

---
+++ Hamkare.Demo/Controller/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Hamkare.Common.Entities;
+using Hamkare.Infrastructure;
+using Hamkare.Service.Services.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hamkare.Demo.Controller;
+
+public class HomeController(RootService<Hotel,ApplicationDbContext> HotelService, RootService<HotelRoom,ApplicationDbContext> HotelRoomService) : Microsoft.AspNetCore.Mvc.Controller
+{
+    [HttpGet("/")]
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        return View();
+    }
+    
+    [HttpGet("/Hotel/[id]")]
+    public async Task<IActionResult> Hotel(long id, CancellationToken cancellationToken)
+    {
+        return View();
+    }
+}
